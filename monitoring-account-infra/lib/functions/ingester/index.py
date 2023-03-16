dffac258de806deea516c3ca95354a8eebd1c4fc
Jie Dong <dongj@amazon.com>
2023-03-16 11:12:59 +1100

    Add job name to emf log property

--- monitoring-account-infra/lib/functions/ingester/index.py
+++ monitoring-account-infra/lib/functions/ingester/index.py
@@ -9,7 +9,7 @@ from constants import SAGEMAKER_STAGE_CHANGE_EVENT
 
 dynamodb = boto3.resource('dynamodb')
 cw = boto3.client('cloudwatch')
-table = dynamodb.Table(os.getenv('JOBHISTORY_TABLE'))
+# table = dynamodb.Table(os.getenv('JOBHISTORY_TABLE'))
 
 @metric_scope
 def lambda_handler(event, context, metrics):
@@ -27,19 +27,21 @@ def lambda_handler(event, context, metrics):
         account = event["account"]
         detail = event["detail"]
         
-        item = {"pk": event_type.name, "account": account, "metadata": json.dumps(detail)}
+        # item = {"pk": event_type.name, "account": account, "metadata": json.dumps(detail)}
         metrics.set_dimensions({"account": account, "jobType": event_type.name}, use_default=False)
         metrics.set_property("JobType", event_type.value)
         metrics.set_property("EMF_LOG", True)
         
         if event_type == SAGEMAKER_STAGE_CHANGE_EVENT.PROCESSING_JOB:
-            item["sk"] = detail.get("ProcessingJobName")
+            # item["sk"] = detail.get("ProcessingJobName")
             job_status = detail.get("ProcessingJobStatus")
-            item["status"] = job_status
+            # item["status"] = job_status
+            metrics.set_property("ProcessingJobName", detail.get("ProcessingJobName"))
             metrics.set_property("ProcessingJobArn", detail.get("ProcessingJobArn"))
 
             if detail.get("FailureReason"):
-                item["failureReason"] = detail.get("FailureReason")
+                metrics.set_property("FailureReason", detail.get("FailureReason"))
+                # item["failureReason"] = detail.get("FailureReason")
 
             if job_status and job_status != "InProgress":
                 metrics.put_metric("ProcessingJobCount_Total", 1, "Count")
@@ -48,13 +50,15 @@ def lambda_handler(event, context, metrics):
                 
 
         elif event_type == SAGEMAKER_STAGE_CHANGE_EVENT.TRAINING_JOB:
-            item["sk"] = detail.get("TrainingJobName")
+            # item["sk"] = detail.get("TrainingJobName")
             job_status = detail.get("TrainingJobStatus")
-            item["status"] = job_status
+            # item["status"] = job_status
+            metrics.set_property("TrainingJobName", detail.get("TrainingJobName"))
             metrics.set_property("TrainingJobArn", detail.get("TrainingJobArn"))
 
             if detail.get("FailureReason"):
-                item["failureReason"] = detail.get("FailureReason")
+                metrics.set_property("FailureReason", detail.get("FailureReason"))
+                # item["failureReason"] = detail.get("FailureReason")
             
             if job_status and job_status != "InProgress":
                 metrics.put_metric("TrainingJobCount_Total", 1, "Count")
@@ -68,16 +72,16 @@ def lambda_handler(event, context, metrics):
                     start_time=datetime.datetime.utcfromtimestamp(float(detail.get("TrainingStartTime"))/1000),
                     end_time=datetime.datetime.utcfromtimestamp(float(detail.get("TrainingEndTime"))/1000)
                 )
-                item["utilization"] = job_metrics
+                # item["utilization"] = job_metrics
                 for metric_name, metric_value in job_metrics.items():
                     metrics.put_metric("TrainingJob_"+metric_name, metric_value, "Percent")
         else:
             print("Unhandled event type")
         
-        table.put_item(
-            Item=item,
-            ReturnValues='NONE'
-        )
+        # table.put_item(
+        #     Item=item,
+        #     ReturnValues='NONE'
+        # )
         
     return None
 
