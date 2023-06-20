d17d78218a61dd2167016e22e68267807fd471e7
Jie Dong <dongj@amazon.com>
2023-06-20 13:01:25 +1000

    emit job metric for processing job

--- monitoring-account-infra/lib/functions/ingester/index.py
+++ monitoring-account-infra/lib/functions/ingester/index.py
@@ -68,11 +68,14 @@ def lambda_handler(event, context, metrics):
                             start_time=datetime.utcfromtimestamp(float(detail.get("CreationTime"))/1000),
                             end_time=datetime.utcfromtimestamp(float(detail.get("ProcessingEndTime"))/1000)
                         )
-                        for host, metrics in job_metrics.items():
-                            job_host_detail = job_detail.copy()
-                            job_host_detail["Host"] = host
-                            job_host_detail["Metrics"] = metrics
-                            print(job_host_detail)
+                        if job_metrics:
+                            for host, metrics in job_metrics.items():
+                                job_host_detail = job_detail.copy()
+                                job_host_detail["Host"] = host
+                                job_host_detail["Metrics"] = metrics
+                                print(job_host_detail)
+                        else:
+                            print(job_detail)
 
             elif event_type == SAGEMAKER_STAGE_CHANGE_EVENT.TRAINING_JOB:
                 job_status = detail.get("TrainingJobStatus")
